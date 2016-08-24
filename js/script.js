2c5ec8e77af824c1e8163bf44fb6929aa53b0312
Maya Angelica Hernandez <mayaah@berkeley.edu>
2016-08-23 17:03:12 -0700

    got rid of section, finally fixed nav bar
    - need to add project pics
    - need to add members section
    - need to make site responsive

--- js/script.js
+++ js/script.js
@@ -11,6 +11,64 @@ $('video').on('ended', function () {
   this.play();
 });
 
+// $(document).ready(function(){
+
+   // $("section").mouseenter(function(){
+     // var id = $(this).attr('id');
+     // $('.sidebar-item .sidebar-line').css({"width": "50%", "background": "#5D6771"});
+     // var linename = "span#"+id+"-line.sidebar-line";
+     // $(linename).css({"width": "100%", "background": "#1DB3E2"});
+   // });
+
+// });
+
+$(document).ready(function(){
+  // $sections incleudes all of the container divs that relate to menu items.
+  var $sections = $('section');
+  
+  // The user scrolls
+  $(window).scroll(function(){
+    
+    // currentScroll is the number of pixels the window has been scrolled
+    var currentScroll = $(this).scrollTop();
+    
+    // $currentSection is somewhere to place the section we must be looking at
+    var $currentSection
+    
+    // We check the position of each of the divs compared to the windows scroll positon
+    $sections.each(function(){
+      // divPosition is the position down the page in px of the current section we are testing      
+      var divPosition = $(this).offset().top;
+      
+      // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+      // the -1 is so that it includes the div 1px before the div leave the top of the window.
+      if( divPosition - 1 < currentScroll ){
+        // We have either read the section or are currently reading the section so we'll call it our current section
+        $currentSection = $(this);
+        
+        // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+      }
+      if ($currentSection.attr('id') == "team"){
+        var id = $currentSection.attr('id');
+        $('.sidebar-item .sidebar-line').css({"width": "50%", "background": "white"});
+         $('.sidebarhover').css({"color": "white"});
+        var linename = "span#"+id+"-line.sidebar-line";
+        $(linename).css({"width": "100%", "background": "white"});
+      }
+      else {
+      // This is the bit of code that uses the currentSection as its source of ID
+     var id = $currentSection.attr('id');
+     $('.sidebar-item .sidebar-line').css({"width": "50%", "background": "#5D6771"});
+     var linename = "span#"+id+"-line.sidebar-line";
+     $(linename).css({"width": "100%", "background": "#1DB3E2"});
+     $('.sidebarhover').css({"color": "#1DB3E2"});
+   }
+      
+    })
+
+  });
+});
+
 (function($) {
 
   /**
